45009c227bcf4b5da07f814cd16ebe92be6de4cf
layash <layashg@gmail.com>
2024-07-26 10:54:04 +0545

    Add basic nav bar for easy navigation

---
+++ include/navBar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand navbar-light bg-light mb-3">
+    <a class="navbar-brand" href="#"></a>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/views/create_eventForm.php">New</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/views/list_events.php">List</a>
+            </li>
+            
+        </ul>
+    </div>
+</nav>

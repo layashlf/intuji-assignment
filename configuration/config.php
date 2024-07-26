8858ef21e019be10934fe4f5645d5d2c16bb8555
layash <layashg@gmail.com>
2024-07-26 10:51:54 +0545

    Create configuration file to connect to google api and authentication

---
+++ configuration/config.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+$client = new Google\Client();
+$client->setAuthConfig(__DIR__ . '/../secrets/client_secret_json.json');
+$client->addScope(['https://www.googleapis.com/auth/calendar']);
+$client->setRedirectUri('http://localhost');
+
+$service = new Google_Service_Calendar($client);
+
+if (isset($_GET['code'])) {
+
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $_SESSION['access_token'] = $token;
+    header('Location: ' . filter_var($client->getRedirectUri(), FILTER_SANITIZE_URL));
+    exit;
+}
+
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+
+    $client->setAccessToken($_SESSION['access_token']);
+} else {
+      
+    $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+    $_SERVER['REQUEST_URI'] != '/' ? header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)) : '';
+}
+?>
--- index.php
+++ index.php
@@ -1,37 +1,53 @@
-<?php 
+<?php
 include ("./include/header.php");
+include ("./configuration/config.php");
 
 ?>
 
-<div class="container mt-10">
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">List event</h5>
-                    <p class="card-text">Click this link to list all upcoming events from your Google Calendar.</p>
-                    <a href="./views/list_events.php" class="btn btn-primary">List</a>
-                </div>
-            </div>
-        </div>
+<div class="container ">
+    <?php if (!$client->getAccessToken()): ?>
+        <?php $authUrl = $client->createAuthUrl(); ?>
+        <div class="container">
 
-        <div class="col-sm-6 col-md-4">
-            <div class="card">
+            <div class="card  col-md-12">
+
                 <div class="card-body">
-                    <h5 class="card-title">Create Event</h5>
-                    <p class="card-text">Click this link to create a new event in your Google Calendar.</p>
-                    <a href="./views/create_eventForm.php" class="btn btn-primary">Create </a>
+                    <h5 class="card-title">Connect your Account</h5>
+                    <p class="card-text"> Click this link to connect your Google Calendar account.</p>
+                    <a href='<?php echo $authUrl ?>' class="btn btn-primary">Connect</a>
                 </div>
             </div>
         </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Disconnect</h5>
-                    <p class="card-text"> Click this link to disconnect your Google Calendar account.</p>
-                    <a href="./events/disconnect.php" class="btn btn-danger">Proceed</a>
+    <?php else: ?>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">List event</h5>
+                        <p class="card-text">Click this link to list all upcoming events from your Google Calendar.</p>
+                        <a href="./views/list_events.php" class="btn btn-primary">List</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Create Event</h5>
+                        <p class="card-text">Click this link to create a new event in your Google Calendar.</p>
+                        <a href="./views/create_eventForm.php" class="btn btn-primary">Create </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Disconnect</h5>
+                        <p class="card-text"> Click this link to disconnect your Google Calendar account.</p>
+                        <a href="./events/disconnect.php" class="btn btn-danger">Proceed</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
 </div>
